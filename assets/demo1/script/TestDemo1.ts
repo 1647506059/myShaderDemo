164cec852f8853774138d4af3d08b7c9d9c54630
dreamly_life <1647506059@qq.com>
2021-08-19 23:40:51 +0800

    提交第二个shader效果：电视机雪花

---
+++ assets/demo1/script/TestDemo1.ts
@@ -0,0 +1,25 @@
+// Learn TypeScript:
+//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
+// Learn Attribute:
+//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
+// Learn life-cycle callbacks:
+//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
+
+const { ccclass, property } = cc._decorator;
+
+@ccclass
+export default class TestDemo1 extends cc.Component {
+
+    @property({ type: cc.Node, displayName: '渲染图片' })
+    ui_sprite: cc.Node = null;
+
+    // LIFE-CYCLE CALLBACKS:
+
+    // onLoad () {}
+
+    start() {
+
+    }
+
+    // update (dt) {}
+}
